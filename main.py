7a3f888fdfacb078dd370d623d872a14f3f5a064
YokiThive <thivedraan0209@gmail.com>
2026-02-18 18:16:45 +0800

    Rate Limit patch

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import discord
 from discord.ext import commands
 import logging
+import sys
+import time
+import traceback
 
 import threading
 from flask import Flask
 import os
+
+from google.api_core.exceptions import exception_class_for_grpc_status
 
 from config import DISCORD_TOKEN, setup_logging
 import events
@@ -34,6 +39,33 @@ def start_server():
 
     port = int(os.environ.get("PORT", "10000"))
     app.run(host="0.0.0.0", port=port)
+
+def retry_bot(bot, handler):
+    retry_delay = 300
+    max_delay = 3600
+    current_attempt = 0
+    max_attempts = 5
+
+    while current_attempt < max_attempts:
+        try:
+            print(f"Attempt #{current_attempt + 1}")
+            bot.run(DISCORD_TOKEN, log_handler=handler, log_level=logging.INFO)
+            break
+
+        except discord.errors.HTTPException as e:
+            if e.status == 429: #limit
+                current_attempt += 1
+                if current_attempt >= max_attempts:
+                    sys.exit(1)
+                time.sleep(retry_delay)
+                retry_delay = min(retry_delay * 2, max_delay)
+            else:
+                print(f"HTTP error: {e}")
+                raise
+
+        except Exception as e:
+            print(f"Error: {e}")
+            raise
 
 if __name__ == "__main__":
     threading.Thread(target=start_server, daemon=True).start()
@@ -73,6 +105,15 @@ if __name__ == "__main__":
         await ctx.send(f"Command Error: {error}")
         print(f"Command Error: {error}")
 
-    handler = setup_logging()
+    @bot.event
+    async def on_error(event, *args, **kwargs):
+        error = sys.exc_info()
+        if error[0] is discord.errors.HTTPException:
+            if error[1].status == 429:
+                print(f"Rate limit exceeded during {event}")
+        traceback.print_exception(*error)
 
-    bot.run(DISCORD_TOKEN, log_handler=handler, log_level=logging.DEBUG)
+    handler = setup_logging()
+    retry_bot(bot, handler)
+
+    #bot.run(DISCORD_TOKEN, log_handler=handler, log_level=logging.DEBUG)
